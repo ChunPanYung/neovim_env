e547200f134713a9492aa99ebce3804cb321329d
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2023-08-04 10:36:55 -0400

    Re-arrange for readability.

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -5,19 +5,20 @@ return {
     opts = function()
       local nls = require("null-ls")
       return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
         sources = {
+          -- shell script
+          nls.builtins.diagnostics.shellcheck,
+          nls.builtins.formatting.shfmt,
+
           nls.builtins.diagnostics.hadolint, -- dockerfile
-          nls.builtins.diagnostics.shellcheck, -- shell script
           nls.builtins.diagnostics.standardjs,
           nls.builtins.diagnostics.yamllint,
 
+          -- python
           nls.builtins.formatting.black,
           nls.builtins.formatting.ruff,
-          nls.builtins.formatting.standardjs,
 
           nls.builtins.formatting.stylua, -- lua formatting
-          nls.builtins.formatting.shfmt,
         },
       }
     end,
