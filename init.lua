648f4ec8a544a15d983990e7fb6d44bd49b0e077
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2023-08-13 20:18:21 -0400

    Move commands to its own file.

--- init.lua
+++ init.lua
@@ -109,22 +109,3 @@ vim.cmd([[
 " autoread and load file when focus on buffer
 au FocusGained,BufEnter * :silent! !
 ]])
-
---[[ User Created Commands ]]
-vim.api.nvim_create_user_command('Trimblank', function()
-  vim.cmd([[
-    " Replace multiple blank lines with a single blank line
-    silent! %s/\(\n\n\)\n\+/\1/
-    " Trim final newline
-    silent! v/\_s*\S/d
-    " Clear all highlight
-    nohlsearch
-  ]])
-end, {})
-
-vim.api.nvim_create_user_command('Trimspace', function()
-  vim.cmd([[
-    " Remove trailing white spaces
-    %s/\s\+$//e
-  ]])
-end, {})
