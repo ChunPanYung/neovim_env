f72a61366f21702214a45134de141719136bb734
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2023-10-01 21:30:40 -0400

    feat: update to 3.x version.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,89 +1,97 @@
 return {
   {
     'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    dependencies = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
+    branch = 'v3.x',
+    lazy = true,
+    config = false,
+    init = function()
+      -- Disable automatic setup, we are doing it manually
+      vim.g.lsp_zero_extend_cmp = 0
+      vim.g.lsp_zero_extend_lspconfig = 0
+    end,
+  },
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},         -- Required
-      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
       {'hrsh7th/cmp-buffer'},       -- Optional
       {'hrsh7th/cmp-path'},         -- Optional
       {'saadparwaiz1/cmp_luasnip'}, -- Optional
       {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
+      {'L3MON4D3/LuaSnip'},
       {'rafamadriz/friendly-snippets'}, -- Optional
-
-      {'folke/neodev.nvim'},
     },
     config = function()
-      require('neodev').setup({})
-      -- Learn the keybindings, see :help lsp-zero-keybindings
-      -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
-      local lsp = require('lsp-zero')
-      lsp.preset('recommended')
+      -- Here is where you configure the autocompletion settings.
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_cmp()
 
-      -- Setup vscode like key mappings
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      lsp.setup_nvim_cmp({
-        mapping = lsp.defaults.cmp_mappings({
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-          ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
+      -- And you can configure cmp even more, if you want to.
+      local cmp = require('cmp')
+      local cmp_action = lsp_zero.cmp_action()
+
+      cmp.setup({
+        sources = {
+          { name = 'buffer' },
+          { name = 'path' },
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' }
+        },
+        formatting = lsp_zero.cmp_format(),
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          ['<Tab>'] = cmp_action.luasnip_supertab(),
+          ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+        }),
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        }
+      })
+    end
+  },
+
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    cmd = 'LspInfo',
+    event = {'BufReadPre', 'BufNewFile'},
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+    },
+    config = function()
+      -- This is where all the LSP shenanigans will live
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_lspconfig()
+
+      lsp_zero.on_attach(function(_, bufnr)
+        -- see :help lsp-zero-keybindings
+        -- to learn the available actions
+        lsp_zero.default_keymaps({
+          buffer = bufnr,
+          preserve_mappings = false
         })
-      })
-      -- Disable set_lsp_keymaps
-      lsp.default_keymaps({
-        -- buffer = bufnr,
-        omit = {'<F3>'}
-      })
+      end)
 
-      -- See :help lsp-zero-preferences
-      lsp.set_preferences({
-        set_lsp_keymaps = true, -- set to false if you want to configure your own keybindings
-        manage_nvim_cmp = true, -- set to false if you want to configure nvim-cmp on your own
+      -- (Optional) Configure lua language server for neovim
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
 
-        -- Opt-out of mason.nvim
-        suggest_lsp_servers = false,
-        setup_servers_on_start = false,
-        call_servers = 'global'
-      })
-      local lsp_opts = {
-        single_file_support = false
-      }
-      lsp.setup_servers({'ruff_lsp', 'rust_analyzer',
+      lsp_zero.setup_servers({'ruff_lsp', 'rust_analyzer',
         -- Web Development
         'quick_lint_js', 'tsserver',
         -- DevOps
         'ansiblels', 'terraformls',
-        opts = lsp_opts})
-
-      -- (Optional) Configure lua language server for neovim
-      lsp.nvim_workspace()
-
-      lsp.setup()
+      })
     end
   }
 }
