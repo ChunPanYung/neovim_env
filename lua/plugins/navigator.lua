142837bccdb156747acdefdc7773847c9df0af09
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2023-10-01 21:31:54 -0400

    feat: add navigator plugins.

---
+++ lua/plugins/navigator.lua
@@ -0,0 +1,10 @@
+return {
+  'ray-x/navigator.lua',
+  enabled = false,
+  dependencies = {
+    {'ray-x/guihua.lua'},
+    {'neovim/nvim-lspconfig'},
+    {'nvim-telescope/telescope.nvim'}
+  },
+  config = true
+}
