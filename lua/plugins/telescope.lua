6d8564e285ba243e6c7350187dda6362308d2a3a
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2023-10-24 10:00:41 -0400

    feat: add keymap for listing buffers.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
     keys = {
       { "<Leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files"},
       { "<Leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Search file contents"},
+      { "<Leader>fb", "<cmd>Telescope buffers<cr>", desc = "List buffers"},
       { "<Leader>ft", "<cmd>lua require('telescope.builtin').treesitter()<cr>", desc = "List functions and variables from treesitter"}
     }
   },
