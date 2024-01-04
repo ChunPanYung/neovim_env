729deb93f8aa76e47ea4de58eaba76da01aca38e
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-01-04 14:15:51 -0500

    style: format so lines won't be too long.

--- lua/plugins/buffer.lua
+++ lua/plugins/buffer.lua
@@ -27,9 +27,27 @@ return {
     enabled = true,
     -- stylua: ignore
     keys = {
-      { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
-      { "<leader>q", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
-      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
-    },
-  },
+      {
+        "<leader>bd",
+        function()
+          require("mini.bufremove").delete(0, false)
+        end,
+        desc = "Delete Buffer"
+      },
+      {
+        "<leader>q",
+        function()
+          require("mini.bufremove").delete(0, false)
+        end,
+        desc = "Delete Buffer"
+      },
+      {
+        "<leader>bD",
+        function()
+          require("mini.bufremove").delete(0, true)
+        end,
+        desc = "Delete Buffer (Force)"
+      },
+    },  -- end keys
+  },  -- end buffer remove
 }
