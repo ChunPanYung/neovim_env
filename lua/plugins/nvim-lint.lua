2577ebda4814baf59ded676442ef60d937bc73b3
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-02-21 14:02:28 -0500

    refactor: reduce complexity.

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -2,7 +2,6 @@ return {
   {
     'mfussenegger/nvim-lint',
     enabled = true,
-    event = "InsertLeave",
     opts = {
       events = { "BufWritePost", "BufReadPost", "InsertLeave" },
       linters_by_ft = {
@@ -12,31 +11,12 @@ return {
     config = function(_, opts)
       local lint = require("lint")
 
-      for name, linter in pairs(opts.linters) do
-        if type(linter) == "table" and type(lint.linters[name]) == "TABLE" then
-          lint.linters[name] = vim.tbl_deep_extend("force", lint.linters[name], linter)
-        else
-          lint.linters[name] = linter
-        end
-      end
       lint.linters_by_ft = opts.linters_by_ft
 
-      lint.try_lint()
-
-      local M = {}
-      function M.debounce(ms, fn)
-        local timer = vim.loop.new_timer()
-        return function(...)
-          local argv = { ... }
-          timer:start(ms, 0, function()
-            timer:stop()
-            vim.schedule_wrap(fn)(unpack(argv))
-          end)
-        end
-      end
       vim.api.nvim_create_autocmd(opts.events, {
-        group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-        callback = M.debounce(100, M.lint),
+        callback = function()
+          lint.try_lint()
+        end
       })
     end
   }
