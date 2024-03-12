9396e691ba1fd0d21924eaaac84809bf4467b1a2
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-03-11 23:19:45 -0400

    feat!: disable neo-tree.

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -8,32 +8,6 @@ return {
     version = "*",
     config = true
   },
-  { -- File explorer
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      close_if_last_window = true,
-      window = {
-        width = 28
-      },
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          never_show = {
-            ".git"
-          }
-        }
-      }
-    },
-    keys = {
-      { "<Leader>e", "<cmd>Neotree toggle<cr>", desc = "Neotree" }
-    }
-  },
   { -- Statusline
     'nvim-lualine/lualine.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
