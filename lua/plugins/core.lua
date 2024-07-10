2a08f7e3cc866dc5bb0b00fbb0af8130b7cd4d06
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-07-10 17:40:24 -0400

    fix: update trouble.nvim key mappings.

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -44,10 +44,39 @@ return {
   },
   { -- Showing diagnostics, references, telescope results, quickfix etc.
     "folke/trouble.nvim",
-    dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = {},
+    cmd = "Trouble",
     keys = {
-      { "<Leader>xx", "<cmd>TroubleToggle<cr>", desc = "Toggle Trouble.nvim" },
-      { "gR", "<cmd>TroubleToggle lsp_references<cr>", desc = "Toggle references" }
+      {
+        "<Leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "diagnostics (Trouble)"
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
     }
   },
   {
