e9f1c208ebe3838061d1d24fff93fd0470c3831c
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-07-10 20:29:27 -0400

    feat!: remove mini.bufremove plugins.

--- lua/plugins/buffer.lua
+++ lua/plugins/buffer.lua
@@ -16,38 +16,5 @@ return {
       {"<S-h>", ":BufferLineCyclePrev<CR>", desc = "Prev Buffer"},
       {"<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers"}
     },
-    opts = {
-      options = {
-        close_command = function(n) require("mini.bufremove").delete(n, false) end,
-        right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end
-      }
-    }
   },
-  { -- buffer remove
-    "echasnovski/mini.bufremove",
-    enabled = true,
-    -- stylua: ignore
-    keys = {
-      {
-        "<leader>bd",
-        function()
-          require("mini.bufremove").delete(0, false)
-        end,
-        desc = "Delete Buffer"
-      },
-      {
-        "<leader>bD",
-        function()
-          require("mini.bufremove").delete(0, true)
-        end,
-        desc = "Delete Buffer (Force)"
-      },
-      {
-        "<leader>bz",
-        ":w<CR><leader>q",
-        desc = "Save the quit current buffer.",
-        remap = true
-      }
-    },  -- end keys
-  },  -- end buffer remove
 }
