fba8535176bffabf46849c35962f02b8f3013737
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-07-12 22:08:15 -0400

    refactor: define description on key itself rather than plugin.

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,25 +2,15 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    config = function()
-      local wk = require("which-key")
-      wk.register({
-        mode = { "n", "v" },
-        ["g"] = { name= "+goto" },
-        [","] = {
-          i = { name = "insert single character before cursor." },
-          a = { name = "insert single character after cursor." }
-        },
-        ["z"] = { name = "+fold" },
-        ["<Leader>"] = {
-          name="+leader",
-          zz = { name = "Write and delete buffer." },
-          bd = { name = "Delete buffer." },
-          q = { name = "Delete buffer." },
-        }
-
-      })
-
-    end
+    opts = {},
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-keys").show({global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      }
+    }
   },
 }
